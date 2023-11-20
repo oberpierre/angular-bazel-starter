32666019d7e0e71f0a4ce9886734bc5c526bbacc
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-11-20 08:12:08 +0000

    F Enabling build and test

---
+++ tools/test-setup.ts
@@ -0,0 +1,11 @@
+import {} from 'jasmine';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
